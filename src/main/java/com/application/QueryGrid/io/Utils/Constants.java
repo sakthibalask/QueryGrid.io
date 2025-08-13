f2b889eade070709ebdd2b31d8227ab7c8efc0e2
sakthibalankathalingam <cseskct211sakthibala.sk@gmail.com>
2025-08-13 18:43:06 +0530

    QG_feat: Removing confidential information.

--- src/main/java/com/application/QueryGrid/io/Utils/Constants.java
+++ src/main/java/com/application/QueryGrid/io/Utils/Constants.java
@@ -2,9 +2,10 @@ package com.application.QueryGrid.io.Utils;
 
 public class Constants {
 
-    public static final String SECRET_KEY =  "ZA6dMSi3Bpaoxg4PxfbGcJzQEd+A7u03Qk4m1gBV83mjhR75lZWjPcPc0f2Di0l0ftOT9XTXxzNyn7IO2CpWIgJdAFnZ7cf8wYyDQoTpoyhV742F4usfk2RuetH4rAWpqZqE0XnTYC57lf3mD+HDTfRUSnRlkNRXWuetm9wNFkzvsoEn3F0L1/CXfV0SjhUs9BsOCJGWw+C3S/IUlNW3RuIBbZ/talMeurKaQpAxiBuFS5xaL/yP0P7M67brvqT6DdwGx0dXCPaMXm0Fz9PYZP775XjwaZcwyLDxLKqrdDUjExOtxeMRECl7xhJBDd+etepFKxJhIqP21jK9r3HVNG3LzS3mFMGnKsbs4QRyWVY=";
+    public static final String SECRET_KEY =  "";
     public static final Integer ACCESS_TOKEN_EXPIRTATION = 86400000;
-    public static final Integer REFRESH_TOKEN_EXPIRTATION = 604800000;
+    public static final String PUBLIC_KEY = "";
+    public static final String PRIVATE_KEY = "";
 
     public static final String[] WHITELIST_URL = {
             "/app/v1/auth/**",
